8077cc51662cdbb6d3b382c1b9f699ae7bce6e74
Amir Afunian <amirafunian@outlook.com>
2021-10-08 20:07:51 -0700

    Added genres

--- Book.cpp
+++ Book.cpp
@@ -4,26 +4,32 @@
 
 #include <iostream>
 #include <ctype.h>
+#include <vector>
 using namespace std;
 
+enum class Genre {
+    fiction, nonfiction, periodical, biography, children
+};
 class Book {
 private:
     string isbn;
     string title;
     string author;
     string copyright;
+    Genre type;
     bool available = true;
 public:
-    Book(string isbn, string title, string author, string copyright);
+    Book(string isbn, string title, string author, string copyright, Genre type);
     void check_in();
     void check_out();
     string get_isbn();
     string get_title();
     string get_author();
     string get_copyright();
+    Genre get_genre();
     bool validISBN(string);
 };
-Book::Book(string i, string t, string a, string c) {
+Book::Book(string i, string t, string a, string c, Genre g) {
 
     if (validISBN(i)) isbn = i;
     else return;
@@ -31,6 +37,7 @@ Book::Book(string i, string t, string a, string c) {
     title = t;
     author = a;
     copyright = c;
+    type = g;
     cout << "Book listed!\n";
 }
 void Book::check_in() {
@@ -53,9 +60,21 @@ string Book::get_author() {
 string Book::get_copyright() {
     return copyright;
 }
-ostream& operator<<(ostream& z, Book x) {
-    cout << "Title: " << x.get_title() << ", Author: " << x.get_author() << ", ISBN: " << x.get_isbn() << '\n';
-    return z << '\n';
+Genre Book::get_genre() {
+    return type;
+}
+ostream& operator<<(ostream& os, Genre g) {
+    vector<string> store { "Fiction", "Nonfiction", "Periodical", "Biography", "Children" };
+    for (int i = 0; i < store.size(); i++) {
+        if (int(g) == i) {
+            cout << store[i];
+            return os;
+        }
+    }
+}
+ostream& operator<<(ostream& os, Book x) {
+    cout << "Title: " << x.get_title() << ", Author: " << x.get_author() << ", Genre: " << x.get_genre() << ", ISBN: " << x.get_isbn() << '\n';
+    return os << '\n';
 }
 bool operator==(Book x, Book y) {
     if (x.get_isbn() == y.get_isbn()) {
@@ -121,9 +140,9 @@ bool Book::validISBN(string i) {
 
 int main()
 {
-    Book test("39-66-73-K", "Amir's Book", "Amir Afunian", "2007");
+    Book test("39-66-73-K", "Amir's Book", "Amir Afunian", "2007", Genre::nonfiction);
     cout << test;
-    Book test2("54-2-973-z", "Joe's Book", "Joe Smith", "1988");
+    Book test2("54-2-973-z", "Joe's Book", "Joe Smith", "1988", Genre::periodical);
     cout << test2;
     test != test2;
 }
